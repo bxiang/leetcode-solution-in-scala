cf3c9caf584c19113c71f50b729fa10e8f2640b3
Brian Xiang <brian.xiang@capco.com>
2019-05-02 21:39:09 -0400

    WIP

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-Leetcode Solutions in Scala
-Just for fun
+#Leetcode Solutions in Scala
+...just for fun
 
-121. Best Time to Buy and Sell Stock
+|  ID   | Problem description                             | Solution |
+| :---- | :---------------------------------------------: | -------: |
+| 121  | [Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock)| [solution](https://github.com/bxiang/leetcode-solution-in-scala/master/src/Problem121.scala) |
+| 784  | [Letter Case Permutation](https://leetcode.com/problems/letter-case-permutation)| [solution](https://github.com/bxiang/leetcode-solution-in-scala/master/src/Problem784.scala) |
+
---
+++ src/test/scala/leetcode/Problem784.scala
@@ -0,0 +1,29 @@
+package leetcode
+
+import org.scalatest.FunSuite
+
+// https://leetcode.com/problems/letter-case-permutation/description/
+class Problem784 extends FunSuite {
+
+  def doCasePermut(input: List[Char]): List[List[Char]] = {
+    input match {
+      case Nil => List.empty
+      case x :: Nil =>
+        if (x.isLetter) List(List(x.toUpper), List(x.toLower)) else (List(List(x)))
+      case x :: xs =>
+        val charList = if (x.isLetter) List(x.toUpper, x.toLower) else List(x)
+        for {
+          str <- doCasePermut(xs)
+          c <- charList
+        } yield c :: str
+    }
+  }
+
+  def letterCasePermutation(input: String): List[String] = doCasePermut(input.toList).map(_.mkString)
+
+  test("letterCasePermutation") {
+    val expectedResult = List("AB3C", "aB3C", "Ab3C", "ab3C", "AB3c", "aB3c", "Ab3c", "ab3c")
+    assert(letterCasePermutation("ab3c") == expectedResult)
+  }
+
+}
